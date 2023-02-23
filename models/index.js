133f0000f0bbf12293f0fce9b58295b91af67449
yogajulianp <yogajprasutiyo@gmail.com>
2023-02-23 13:45:09 +0700

    crud with env

--- models/index.js
+++ models/index.js
@@ -6,8 +6,10 @@ const sequelize = new Sequelize(
     process.env.MYSQL_USER, 
     process.env.MYSQL_PASSWORD, 
     {
-    dialect: 'mysql',
-    dialectOptions: {
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT,
+      dialect: 'mysql',
+      dialectOptions: {
       // Your mysql2 options here
     },
   });
